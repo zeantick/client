dc37db6ce23896f600a823ca36282a2c1ec646d4
zeantick <aivan2575@gmail.com>
2026-01-16 11:41:18 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -82,7 +82,8 @@ app.get('/arrival-times', (req, res) => {
     });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
